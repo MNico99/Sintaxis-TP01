5905ffb27a7474d2c3f3f612da75a5704e4e9ca1
Emmanuel <emmanuel_roberto@hotmail.com>
2019-05-21 22:37:04 -0300

    Minimos cambios

--- Lex.py
+++ Lex.py
@@ -1,120 +1,111 @@
 # -*- coding: utf-8 -*-
-from GLex import *
-from GSin import *
+from automatas import *
 
-TRAMPA = -1
-RESULTADO_ACEPTADO = "ACEPTADO"
-RESULTADO_TRAMPA = "TRAMPA"
-RESULTADO_NO_ACEPTADO = "NO_ACEPTADO"
-
-digito = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-letra = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", 
-"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", 
-"F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", 
-"U", "V", "W", "X", "Y", "Z"]
-
-tokens = ["eof", "fun", "(", ")", ",", "var", ";", "=", "if", "for", "else", "return", 
-"while", "{", "}", "or", "and", "==", "!=", ">", "<", "<=", ">=", "-", "+", "/", "*", 
-"!", "true", "false", ".", "'", "Numeros", "ID", "String"]
-
-
-#for posicion,char in enumerate(cadena):
-#    if char != espacio:
-#        lexeme += char # adding a char each time
-#    if (posicion+1 < len(string)): # prevents error
-#       if string[posicion+1] == espacio # if next char == ' '
-#            if lexeme != '':
-#                print(lexeme.replace('\n', '<newline>'))
-#                lexeme = ''
-espacio = ' '
-lexeme = ''
-lexer = []
-cadena = " if else"
-
-ej= cadena.split()
-
-for posicion in range(0, len(ej)):
-    if A_If(ej[posicion]) == RESULTADO_ACEPTADO:
-        lexer += ("IF", ej[posicion])
-    if A_Else(ej[posicion]) == RESULTADO_ACEPTADO:
-        lexer += ("ELSE", ej[posicion])
+#DEVUELVE QUE AUTOMATA ACEPTA LA CADENA INGRESADA
+tokens = []
+def automata(src: str) -> tokens:
     
+    if cadena[0] in digito:
+        A_Num(cadena)
+    if cadena[0].isalpha():
+        A_AND(cadena)
+        A_Else(cadena)
+        A_EOF(cadena)
+        A_False(cadena)
+        A_FOR(cadena)
+        A_FUN(cadena)
+        A_If(cadena)
+        A_OR(cadena)
+        A_Return(cadena)
+        A_True(cadena)
+        A_VAR(cadena)
+        A_While(cadena)
+        A_ID(cadena)
+    if cadena[0] in simbolos:
+        A_Asterisco(cadena)
+        A_Coma(cadena)
+        A_CorcheteAbierto(cadena)
+        A_CorcheteCerrado(cadena)
+        A_DobleIgual(cadena)
+        A_Exclamacion(cadena)
+        A_ExclamacionIgual(cadena)
+        A_Guionmedio(cadena)
+        A_Igual(cadena)
+        A_Mas(cadena)
+        A_Mayor(cadena)
+        A_Mayorigual(cadena)
+        A_Menor(cadena)
+        A_Menorigual(cadena)
+        A_ParenClose(cadena)
+        A_ParenOpen(cadena)
+        A_PuntoComa(cadena)
+        A_Slash(cadena)
+    if cadena[0] == "'":
+        A_Apostrofo(cadena)
+        A_String(cadena)
 
-for posicion,char in enumerate(cadena):
-    if char != espacio:
-        lexeme += char
-        if A_AND(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("AND", lexeme) 
-        if A_Apostrofo(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("'", lexeme)
-        if A_Asterisco(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("*", lexeme)
-        if A_Coma(cadena) == RESULTADO_ACEPTADO:
-            lexer += (",", lexeme)      
-        if A_CorcheteAbierto(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("[", lexeme)    
-        if A_CorcheteCerrado(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("]", lexeme)
-        if A_DobleIgual(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("==", lexeme)
-        if A_Else(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("ELSE", lexeme)
-        if A_EOF(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("EOF", lexeme)
-        if A_Exclamacion(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("!", lexeme)
-        if A_ExclamacionIgual(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("!=", lexeme)
-        if A_False(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("FALSE", lexeme)
-        if A_FOR(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("FOR", lexeme)
-        if A_FUN(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("FUN", lexeme)
-        if A_Guionmedio(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("-", lexeme)
-        if A_ID(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("ID", lexeme)
-        if A_If(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("IF", lexeme)
-        if A_Igual(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("=", lexeme)
-        if A_Mas(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("+", lexeme)
-        if A_Mayor(cadena) == RESULTADO_ACEPTADO:
-            lexer += (">", lexeme)
-        if A_Mayorigual(cadena) == RESULTADO_ACEPTADO:
-            lexer += (">=", lexeme)
-        if A_Menorigual(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("<=", lexeme)
-        if A_Menor(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("<", lexeme)
-        if A_Numeros(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("NUMEROS", lexeme)
-        if A_OR(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("OR", lexeme)
-        if A_ParenClose(cadena) == RESULTADO_ACEPTADO:
-            lexer += (")", lexeme)
-        if A_ParenOpen(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("(", lexeme)
-        if A_Punto(cadena) == RESULTADO_ACEPTADO:
-            lexer += (".", lexeme)
-        if A_PuntoComa(cadena) == RESULTADO_ACEPTADO:
-            lexer += (";", lexeme)
-        if A_Return(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("RETURN", lexeme)
-        if A_Slash(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("/", lexeme)
-        if A_String(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("STRING", lexeme)
-        if A_True(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("TRUE", lexeme)
-        if A_VAR(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("VAR", lexeme)
-        if A_While(cadena) == RESULTADO_ACEPTADO:
-            lexer += ("WHILE", lexeme)
-    if (posicion+1 < len(cadena)): 
-        if cadena[posicion+1] == espacio:
-            if lexeme != '':
-                lexeme = ''
-print(lexer)
+lexeme = ''
+cadena = "9999"
+
+TokenConfig = [
+    ('PALABRAS RESERVADAS', A_If),
+    ('PALABRAS RESERVADAS', A_EOF),
+    ('PALABRAS RESERVADAS', A_Else),
+    ('PALABRAS RESERVADAS', A_False),
+    ('PALABRAS RESERVADAS', A_FOR),
+    ('PALABRAS RESERVADAS', A_FUN),
+    ('PALABRAS RESERVADAS', A_Return),
+    ('PALABRAS RESERVADAS', A_True),
+    ('PALABRAS RESERVADAS', A_VAR),
+    ('PALABRAS RESERVADAS', A_While),
+    ('SIMBOLOS RESERVADOS', A_ParenOpen),
+    ('SIMBOLOS RESERVADOS', A_ParenClose),
+    ('SIMBOLOS RESERVADOS', A_Apostrofo),
+    ('SIMBOLOS RESERVADOS', A_Asterisco),
+    ('SIMBOLOS RESERVADOS', A_Coma),
+    ('SIMBOLOS RESERVADOS', A_CorcheteAbierto),
+    ('SIMBOLOS RESERVADOS', A_CorcheteCerrado),
+    ('SIMBOLOS RESERVADOS', A_Exclamacion),
+    ('SIMBOLOS RESERVADOS', A_Guionmedio),
+    ('SIMBOLOS RESERVADOS', A_Mas),
+    ('SIMBOLOS RESERVADOS', A_Punto),
+    ('SIMBOLOS RESERVADOS', A_PuntoComa),
+    ('SIMBOLOS RESERVADOS', A_Slash),
+    ('OP RELACIONALES', A_DobleIgual),
+    ('OP RELACIONALES', A_ExclamacionIgual),
+    ('OP RELACIONALES', A_Menor),
+    ('OP RELACIONALES', A_Mayor),
+    ('OP RELACIONALES', A_Mayorigual),
+    ('OP RELACIONALES', A_Menorigual),
+    ('OP LOGICOS', A_AND),
+    ('OP LOGICOS', A_OR),
+    ("NUM", A_Num),
+    ("ID", A_ID),
+]
+
+def lex(cadena):
+  tokens = []
+  start = 0
+  index = 0
+  while index <= len(cadena):
+    # tomo un prefijo
+    cadenaPrefijo = cadena[start: index + 1]
+    todosTrampa = True
+    # evaluo la cadena prefijo con cada automata (tiene que definir TokenConfig)
+    for automata, tipoDeToken in TokenConfig: 
+      resultado = automata(cadenaPrefijo)
+      # si resultado NO es resultado trampa
+      # entonces  todosTrampa va a ser False,
+      # por ende podemos consumir mas caracteres, es decir
+      # la cadena prefijo es mas larga
+
+    # fuera del ciclo
+    # si son todosTrampa entonces llegue a  la maxima cadena prefijo
+    # vuelvo index para atras (index -= 1)
+    # y hago tokens.append(nuevoToken)
+    # donde nuevo token va a tener el tipo del primer candidato y el lexeme
+    # es efectivamente cadenaPrefijo
+ 
+    # SINO son todso tramapa simplemente avanzo el puntero
+    #index += 1
+      
